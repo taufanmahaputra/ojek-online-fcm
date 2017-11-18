b7d496c69bb95f85542d341745309ccbedd913cd
taufanmahaputra <hacker.indo62@gmail.com>
2017-11-18 22:49:43 +0700

    firebase connected, send simple message

--- app.js
+++ app.js
@@ -1,8 +1,7 @@
 // importing modules
 require('./app/index');
 
-var admin = require("firebase-admin");
-var serviceAccount = require("./pro-jek-wbd-firebase.json");
+var firebase = require("firebase");
 var express = require('express');
 var mongoose = require('mongoose');
 var bodyparser = require('body-parser');
@@ -11,13 +10,44 @@ var path = require('path');
 
 var app = express();
 
-// init Firebase Admin SDK
+// Initialize Firebase
+var admin = require("firebase-admin");
+
+var serviceAccount = require("./pro-jek-wbd-firebase.json");
+
 admin.initializeApp({
-    credential: admin.credential.cert(serviceAccount),
-    databaseURL: "https://pro-jek-wbd.firebaseio.com"
+  credential: admin.credential.cert(serviceAccount),
+  databaseURL: "https://<DATABASE_NAME>.firebaseio.com"
 });
 
 console.log(admin);
+
+// This registration token comes from the client FCM SDKs.
+var registrationToken = "es3bFtw27ys:APA91bE0-V-KUoE2tL4qK7j5PNB1tV8OgPkHDfmhEvRYEPwvZORIuLK0yoH2xF5Rmibkyq_qf_ZWwxj2pXgrSaP6gsxQ82qoNtPNK3HQbLkXyQZ8maMpteVJ1WI3vh2FYRx3da7A2fmM";
+
+// See the "Defining the message payload" section below for details
+// on how to define a message payload.
+var payload = {
+    notification: {
+        title: "$GOOG up 1.43% on the day",
+        body: "$GOOG gained 11.80 points to close at 835.67, up 1.43% on the day."
+    }
+};
+
+// Send a message to the device corresponding to the provided
+// registration token.
+admin.messaging().sendToDevice(registrationToken, payload)
+  .then(function(response) {
+    // See the MessagingDevicesResponse reference documentation for
+    // the contents of response.
+    console.log("Successfully sent message:", response);
+  })
+  .catch(function(error) {
+    console.log("Error sending message:", error);
+  });
+
+//const messaging = firebase.messaging();
+//console.log(messaging);
 
 // app configuration
 const port = 3000;
